630b60a5405b8a9fffb91c237bfea5e238f08a96
wjdwo951219 <wjdwo951219@gmail.com>
2025-02-26 10:57:52 +0900

    init

--- src/main/java/toy/slick/controller/RootController.java
+++ src/main/java/toy/slick/controller/RootController.java
@@ -1,19 +1,16 @@
 package toy.slick.controller;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import toy.slick.common.Response;
 
-@Hidden
 @RestController
 @RequestMapping("")
 public class RootController {
 
     @GetMapping("/")
-    public Response<HttpStatus> root() {
-        return new Response<>(HttpStatus.OK);
+    public String root() {
+        return HttpStatus.OK.toString();
     }
 }
