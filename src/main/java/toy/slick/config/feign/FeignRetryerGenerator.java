50006f6d1ac8957e5465beec8c4566aba881acde
wjdwo951219 <wjdwo951219@gmail.com>
2025-09-04 09:56:00 +0900

    add feign retry log

--- src/main/java/toy/slick/config/feign/FeignRetryerGenerator.java
+++ src/main/java/toy/slick/config/feign/FeignRetryerGenerator.java
@@ -3,9 +3,11 @@ package toy.slick.config.feign;
 import feign.Request;
 import feign.RetryableException;
 import feign.Retryer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 
+@Slf4j
 public class FeignRetryerGenerator {
 
     public static Retryer create(Duration retryPeriod, Duration retryMaxPeriod, int retryCnt) {
@@ -13,6 +15,11 @@ public class FeignRetryerGenerator {
             @Override
             public void continueOrPropagate(RetryableException e) {
                 if (Request.HttpMethod.GET == e.method()) {
+                    log.info("Retrying : method={}, url={}, reason={}",
+                            e.method(),
+                            e.hasRequest() ? e.request().url() : null,
+                            e.getMessage());
+
                     super.continueOrPropagate(e);
                 } else {
                     throw e;
@@ -22,15 +29,6 @@ public class FeignRetryerGenerator {
     }
 
     public static Retryer create(Duration retryPeriod, int retryCnt) {
-        return new Retryer.Default(retryPeriod.toMillis(), retryPeriod.toMillis(), retryCnt + 1) {
-            @Override
-            public void continueOrPropagate(RetryableException e) {
-                if (Request.HttpMethod.GET == e.method()) {
-                    super.continueOrPropagate(e);
-                } else {
-                    throw e;
-                }
-            }
-        };
+        return create(retryPeriod, retryPeriod, retryCnt);
     }
 }
